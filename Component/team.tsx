3a3d5bd0a3855b95c11eb841d5b979b56d16e185
temuujinnn <temvkaa@gmail.com>
2023-01-10 08:36:00 +0800

    mobile responsive

--- Component/team.tsx
+++ Component/team.tsx
@@ -14,82 +14,61 @@ const teamData = [
     img: "https://media.discordapp.net/attachments/1000991557886947358/1061270650167242822/E8A48002-B273-4BDB-B627-C7D88FDD2757_1_105_c.jpeg",
     name: "Б.Батжин",
     role: "INFLUENCE & COMMUNICATION MENTOR",
-    info: `   Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
-              tempore quas iure libero facere beatae temporibus suscipit
-              quisquam possimus dolorum quis ullam animi quo iste repellat
-              nesciunt doloremque fugiat! Culpa!`,
+    info: `   Батжин нь 2015 оноос хойш бусдад өгье, хуваалцья гэсэн сэтгэлгээгээр өдий хүрсэн бөгөөд өдгөө 12 компани үүсгэсэн, бизнесийн салбарт тэр тусмаа нийгэмд ээлтэй бизнесийг Монгол улсад түгээн дэлгэрүүлж боловсролын технологийн салбарт ТЭСРЭЛТ авчрахаар ажиллаж байна.
+Тэрээр боловсролын салбарт 12 дахь жилдээ ажиллаж байгаа бөгөөд хуримтлуулсан арвин их туршлагаасаа Bit Academy-ийн сурагчдад хуваалцахаар шийдсэн байна.`,
   },
   {
     img: "https://media.discordapp.net/attachments/1000991557886947358/1061270650167242822/E8A48002-B273-4BDB-B627-C7D88FDD2757_1_105_c.jpeg",
     name: "Ө.Тодсүрэн",
     role: "SYSTEM MENTOR",
-    info: `   Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
-              tempore quas iure libero facere beatae temporibus suscipit
-              quisquam possimus dolorum quis ullam animi quo iste repellat
-              nesciunt doloremque fugiat! Culpa!`,
+    info: ` Тодсүрэн нь 2015 оноос хойш инженерчлэлийн салбарт хөл тавьсан, 2021 оноос эхлэн Томүжин Технологи компанийг хамтран үүсгэн байгуулцаж байгаа ELFC, NFT.mn, ELF Wallet, Tiny Battle зэрэг төслүүдийг ахлан хөгжүүлж байгаа бөгөөд Bit Academy-д мэддэг бүхнээ өгөхөөр шийдсэн байна.`,
   },
   {
     img: "https://media.discordapp.net/attachments/1000991557886947358/1061270650167242822/E8A48002-B273-4BDB-B627-C7D88FDD2757_1_105_c.jpeg",
     name: "З.Заяадэлгэр",
     role: "TECHNOLOGY MENTOR",
-    info: `   Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
-              tempore quas iure libero facere beatae temporibus suscipit
-              quisquam possimus dolorum quis ullam animi quo iste repellat
-              nesciunt doloremque fugiat! Culpa!`,
+    info: ` Заяадэлгэр нь 2012 оноос хойш технологийн салбарт ажиллаж байгаа Монгол улсын томоохон системийн нэг EMART, SKY, ONIS зэрэг худалдааны системүүд хөгжүүлэхэд гүйцэтгэх болон багийг удирдан ажиллаж байв. Тэрээр 2019 оноос хойш Томүжин Технологи компанийг хамтран үүсгэн байгуулж өдгөө Yoda, TASS, Tiny Social Learning, Tiny Battle зэрэг боловсролын старт-апуудыг хөгжүүлэн ажиллаж байна.
+Тэрээр өдий олон жил хуримтлуулсан технологийн туршлага, алдаа онооноосоо хуваалцахаар Bit Academy-д үнэ цэнээ түгээхээр шийдсэн байна.`,
   },
   {
     img: "https://media.discordapp.net/attachments/1000991557886947358/1061270650167242822/E8A48002-B273-4BDB-B627-C7D88FDD2757_1_105_c.jpeg",
     name: "О.Анар-Од",
     role: "PROCESS & COMMUNICATION MENTOR",
-    info: `   Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
-              tempore quas iure libero facere beatae temporibus suscipit
-              quisquam possimus dolorum quis ullam animi quo iste repellat
-              nesciunt doloremque fugiat! Culpa!`,
+    info: `Анар-Од нь 2015 оноос хойш Томүжин Академи, ELF EDUCATION, ELF NEXT DOOR зэрэг боловсролын салбарт түүчээлж буй томоохон компаниудыг үүсгэн байгуулцаж байсан арвин их туршлагатай.
+Тэрээр 2021 оноос эхлэн технологийн салбарт үнэ цэнээ хуваалцахаар орж ирсэн бөгөөд арвин их туршлагаасаа хуваалцахаар Bit Academy-д MENTOR-лохоор шийдсэн байна.`,
   },
   {
     img: "https://media.discordapp.net/attachments/1000991557886947358/1061270650167242822/E8A48002-B273-4BDB-B627-C7D88FDD2757_1_105_c.jpeg",
     name: "Б.Төгөлдөр",
     role: "PROCESS MENTOR",
-    info: `   Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
-              tempore quas iure libero facere beatae temporibus suscipit
-              quisquam possimus dolorum quis ullam animi quo iste repellat
-              nesciunt doloremque fugiat! Culpa!`,
+    info: ` Төгөлдөр нь 2021 оноос эхлэн Томүжин Технологи компанийг үүсгэн байгуулцаж байгаа бөгөөд NFT.mn, Tiny Battle төслүүд дээр арт директ хийж хэрэглэгч нарт юуг, яаж хүргэхийг тодорхойлж байна. Тэрээр Bit Academy-д үнэ цэнээ хуваалцахаар болсон байна.`,
   },
   {
     img: "https://media.discordapp.net/attachments/1000991557886947358/1061270650167242822/E8A48002-B273-4BDB-B627-C7D88FDD2757_1_105_c.jpeg",
     name: "Х.Намсрай",
-    role: "TECHNOLOGY & INFLUENCE MENTOR",
-    info: `   Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
-              tempore quas iure libero facere beatae temporibus suscipit
-              quisquam possimus dolorum quis ullam animi quo iste repellat
-              nesciunt doloremque fugiat! Culpa!`,
+    role: `TECHNOLOGY`,
+    info: `Намсрай нь 2021 оноос эхлэн Томүжин Технологи компанийг үүсгэн байгуулцаж байгаа Tiny Social Learning, Tiny Battle зэрэг томоохон старт-апуудыг үүсгэн байгуулцаж, хөгжүүлж, бүтээлцэж байгаа бөгөөд технологийн ертөнц дахь алдаа оноо, туршлагаасаа Bit Academy-ийн сурагчдад хуваалцахаар харц шулуудсан байна`,
   },
   {
     img: "https://media.discordapp.net/attachments/1000991557886947358/1061270650167242822/E8A48002-B273-4BDB-B627-C7D88FDD2757_1_105_c.jpeg",
     name: "Ч.Тэмүүжин",
     role: "TECHNOLOGY & COMMUNICATION MENTOR",
-    info: `   Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
-              tempore quas iure libero facere beatae temporibus suscipit
-              quisquam possimus dolorum quis ullam animi quo iste repellat
-              nesciunt doloremque fugiat! Culpa!`,
+    info: ` Тэмүүжин нь 2021 оноос эхлэн Томүжин Технологи компанийг үүсгэн байгуулцаж байгаа бөгөөд NFT.mn, ELF Wallet зэрэг блокчейн төслүүдийг ахлан гүйцэтгэж байгаа арвин туршлагатай.
+Тэрээр өөрийн цаг заваа зориулан Bit Academy-ийн сурагчиддаа мэддэг, чаддаг бүхнээ зориулахаар шийдсэн байна.`,
   },
   {
     img: "https://media.discordapp.net/attachments/1000991557886947358/1061270650167242822/E8A48002-B273-4BDB-B627-C7D88FDD2757_1_105_c.jpeg",
     name: "Б.Цэрэнтогтох",
     role: "TECHNOLOGY & SYSTEM MENTOR",
-    info: `   Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
-              tempore quas iure libero facere beatae temporibus suscipit
-              quisquam possimus dolorum quis ullam animi quo iste repellat
-              nesciunt doloremque fugiat! Culpa!`,
+    info: `Цэрэнтогтох нь 2021 оноос эхлэн Томүжин Технологи компанийг үүсгэн байгуулцаж байгаа бөгөөд Tiny Social Learning, Tiny Battle зэрэг тоглоомийн төслүүдийн системийн архитектур, логикийг гарган явж байна.
+Тэрээр өөрийн цаг заваа зориулан Bit Academy-ийн сурагчиддаа мэддэг, чаддаг бүхнээ зориулахаар зориж байна.`,
   },
   {
     img: "https://media.discordapp.net/attachments/1000991557886947358/1061270650167242822/E8A48002-B273-4BDB-B627-C7D88FDD2757_1_105_c.jpeg",
     name: "У.Чингүн",
     role: "TECHNOLOGY MENTOR",
-    info: `   Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
-              tempore quas iure libero facere beatae temporibus suscipit
-              quisquam possimus dolorum quis ullam animi quo iste repellat
-              nesciunt doloremque fugiat! Culpa!`,
+    info: `Чингүн нь 9-р ангиасаа эхлэн програмчлал, кодинг, компьютерийн шинжлэх ухааны салбарт гар дүрсэн бөгөөд өдгөө ELFC, NFT.mn, Tiny Battle зэрэг төслүүдийн системийн архитектур, инженерчлэлийг хийж байгаа бөгөөд өөрийн мэдлэг, туршлага, KNOW HOW-оосоо хуваалцахаар Bit Academy-д MENTOR-лохоор болсон байна.
+`,
   },
 ];
 
@@ -138,17 +117,9 @@ const TeamCard = ({ user }: any) => {
           transition={"ease-out 0.3s"}
           opacity={0}
           _hover={{ opacity: 1 }}
-          overflowY="scroll"
+          // overflowY="scroll"
         >
-          <video
-            ref={videoRef}
-            width="100%"
-            // src="https://tassfiles.s3.ap-northeast-2.amazonaws.com/bitacademy/%D0%98%D0%BD%D0%B6%D0%B5%D0%BD%D0%B5%D1%80%D2%AF%D2%AF%D0%B4+%D0%B1%D2%AF%D1%82%D1%8D%D1%8D%D0%B3%D0%B4%D1%8D%D1%85%D2%AF%D2%AF%D0%BD+%D1%85%D3%A9%D0%B3%D0%B6%D2%AF%D2%AF%D0%BB%D1%8D%D1%85+%D2%AF%D0%B5%D0%B4+%D0%BA%D0%BE%D0%B4+%D0%B1%D0%B8%D1%87%D0%B8%D0%B6+%D1%8D%D1%85%D0%BB%D1%8D%D1%85%D1%8D%D1%8D%D1%81+%D3%A9%D0%BC%D0%BD%D3%A9+%D1%8E%D1%83+%D1%85%D0%B8%D0%B8%CC%86%D1%85+%D1%85%D1%8D%D1%80%D1%8D%D0%B3%D1%82%D1%8D%D0%B8%CC%86+%D0%B2%D1%8D%3F.MOV"
-            autoPlay
-            loop
-            muted
-            controls
-          />
+          {user?.info}
         </Box>
       </Box>
 
